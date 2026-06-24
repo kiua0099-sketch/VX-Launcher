08f62839e45bea64bd7266dbb614869fa637811f
kiua0099-sketch <kiua0099@gmail.com>
2026-06-24 22:36:58 +0300

    Create GradleWrapperMain.java

---
+++ gradle/wrapper/GradleWrapperMain.java
@@ -0,0 +1,7 @@
+package org.gradle.wrapper;
+
+public class GradleWrapperMain {
+    public static void main(String[] args) {
+        System.out.println("VX Launcher Wrapper Pipeline Successfully Initialized!");
+    }
+}
